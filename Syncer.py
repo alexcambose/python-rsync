2a8ef8afcaf468af879bbff3ab998fad4243bc1b
alexcambose <alexcambose@yahoo.com>
2020-11-30 13:09:10 +0200

    Syncer bug fixes

--- Syncer.py
+++ Syncer.py
@@ -23,14 +23,18 @@ class Syncer:
                 if i < len(state_a) and i < len(state_b) and j < len(state_a) and j < len(state_b):
                     if state_a[i]['path'] == state_b[j]['path'] and state_a[i]['is_directory'] == state_b[j]['is_directory'] and state_a[i]['last_modified'] != state_b[j]['last_modified']:
                         if state_a[i]['last_modified'] < state_b[j]['last_modified']:
-                            self.class_a.copy_from(self.class_b, state_b[i]['path'])
+                            self.class_a.copy_from(
+                                self.class_b, state_b[i]['path'])
                         else:
-                            self.class_b.copy_from(self.class_a, state_a[i]['path'])
+                            self.class_b.copy_from(
+                                self.class_a, state_a[i]['path'])
                     if state_b[i]['path'] == state_a[j]['path'] and state_b[i]['is_directory'] == state_a[j]['is_directory'] and state_b[i]['last_modified'] != state_a[j]['last_modified']:
                         if state_b[i]['last_modified'] < state_a[j]['last_modified']:
-                            self.class_b.copy_from(self.class_a, state_a[i]['path'])
+                            self.class_b.copy_from(
+                                self.class_a, state_a[i]['path'])
                         else:
-                            self.class_a.copy_from(self.class_b, state_b[i]['path'])
+                            self.class_a.copy_from(
+                                self.class_b, state_b[i]['path'])
         (state_a, previous_state_a) = self.class_a.create_state()
         (state_b, previous_state_b) = self.class_b.create_state()
         state_a = remove_dictionary_key(state_a, 'last_modified')
@@ -38,11 +42,13 @@ class Syncer:
         print(state_a)
         print(state_b)
         print('')
+        # copy missing files
         for i in range(max(len(state_a), len(state_b))):
             if i < len(state_a) and state_a[i] not in state_b:
                 self.class_b.copy_from(self.class_a, state_a[i]['path'])
             if i < len(state_b) and state_b[i] not in state_a:
                 self.class_a.copy_from(self.class_b, state_b[i]['path'])
+
     def update(self):
         print('UPDATE')
         (state_a, previous_state_a) = self.class_a.create_state()
@@ -53,7 +59,7 @@ class Syncer:
         modified = False
         for element_b in previous_state_a:
             for element_a in state_a:
-                if element_a['path'] == element_b['path'] and element_b['is_directory'] == element_a['is_directory']:
+                if element_a['path'] == element_b['path'] and element_b['is_directory'] == element_a['is_directory'] and element_a['is_directory'] == False:
                     if element_a['last_modified'] > element_b['last_modified']:
                         self.class_b.copy_from(self.class_a, element_a['path'])
                         modified = True
@@ -74,12 +80,12 @@ class Syncer:
         if modified:
             print('REFRESH')
             self.update()
-            return;
+            return
         modified = False
         # check for differences between the current and previous states
         for element_b in previous_state_b:
             for element_a in state_b:
-                if element_a['path'] == element_b['path'] and element_b['is_directory'] == element_a['is_directory']:
+                if element_a['path'] == element_b['path'] and element_b['is_directory'] == element_a['is_directory'] and element_a['is_directory'] == False:
                     if element_a['last_modified'] > element_b['last_modified']:
                         self.class_a.copy_from(self.class_b, element_a['path'])
                         modified = True
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 def remove_dictionary_key(dictionary_array, key):
     def it(x):
-        x.pop(key)
+        if key in x:
+            x.pop(key)
         return x
     return list(map(it, dictionary_array))
