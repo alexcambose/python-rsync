58d860d65509b4f411055bf4b1e2c32580e850ea
alexcambose <alexcambose@yahoo.com>
2020-11-30 13:09:22 +0200

    Filesystem bug fixes

--- monitors/Filesystem.py
+++ monitors/Filesystem.py
@@ -6,6 +6,7 @@ from StateManager import StateManager
 def log(*content):
     print('[FILESYSTEM] ', *content)
 
+
 class Filesystem:
     def __init__(self, path):
         self.state_manager = StateManager()
@@ -19,7 +20,7 @@ class Filesystem:
             real_path = path.normpath(self.path + dirname)
 
             if index > 0:
-                state.append({'path': dirname, 'is_directory': True, 'last_modified': self.get_last_modified_time(real_path)})
+                state.append({'path': dirname, 'is_directory': True})
             for filename in filenames:
                 path_with_file = path.join(dirname, filename)
                 state.append({'path': path_with_file, 'is_directory': False,
@@ -31,32 +32,37 @@ class Filesystem:
         return math.floor(path.getmtime(path.abspath(filepath)) / 10)
 
     def read(self, filename):
+        filename = path.normpath(self.path + filename)
+        print(filename)
         f = open(filename, "r")
         content = f.read()
         f.close()
         return content
 
     def is_directory(self, filename):
+        filename = path.normpath(self.path + filename)
         return path.isdir(filename)
 
     def create_directory(self, filename):
         return mkdir(filename)
 
     def delete(self, filename):
-        filename = path.normpath(self.path + filename)
-        log('Delete from ', filename)
+        file = path.normpath(self.path + filename)
+        log('Delete from ', file)
         if self.is_directory(filename):
-            return rmdir(filename)
-        return remove(filename)
+            return rmdir(file)
+        return remove(file)
 
     def copy_from(self, class_b, filename):
-        target_path = path.normpath(self.path + filename)
-        from_path = path.normpath(class_b.path + filename)
-        log('Copy from ',from_path, 'to', target_path)
+        target_path = filename
+        from_path = filename
         if class_b.is_directory(from_path):
+            log('Copy from ', from_path, 'to', target_path)
             self.create_directory(target_path)
         else:
+            log('Copy from ', from_path, 'to',
+                path.normpath(self.path + target_path))
             contents = class_b.read(from_path)
-            f = open(target_path, 'w')
+            f = open(path.normpath(self.path + target_path), 'w')
             f.write(contents)
             f.close()
