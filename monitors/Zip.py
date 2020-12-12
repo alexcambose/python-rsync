19e715aacb53509baa754bb9ec0edc8eeee604f3
Alexandru Cambose <alexcambose@yahoo.com>
2020-12-12 16:55:52 +0200

    Zip error handling

--- monitors/Zip.py
+++ monitors/Zip.py
@@ -1,14 +1,15 @@
 """
     Zip class
     """
-from os import walk, path, mkdir, remove, rmdir, rename
+from os import name, walk, path, mkdir, remove, rmdir, rename
 import math
+from utils import handle_failure
 from StateManager import StateManager
 from zipfile import ZipFile, ZipInfo
 import datetime
 import ntpath
 import re
-
+from pathlib import Path
 
 def log(*content):
     print('[ZIP] ', *content)
@@ -31,76 +32,93 @@ class Zip:
             return None
         return x.group(1)
 
+    @handle_failure(log)
     def open_read(self):
         if self.zip_r:
             self.zip_r.close()
         self.zip_r = ZipFile(self.path, 'r')
-
+    
+    @handle_failure(log)
     def open_write(self):
         if self.zip_w:
             self.zip_w.close()
         self.zip_w = ZipFile(self.path, 'a')
-
+    
+    @handle_failure(log)
     def create_state(self):
+        state = []
         self.open_read()
 
         name_list = self.zip_r.namelist()
+        # add nested folders if they doesn't exist
+        for file in name_list:
+            folder_path = str(Path(file).parent) + path.sep
+            if folder_path not in name_list and not self.zip_r.getinfo(file).is_dir() and folder_path != '.' + path.sep:
+                state.append({'path': folder_path[:-1], 'is_directory': True})
         # set current state for class_a
-        state = []
         for file in name_list:
             info = self.zip_r.getinfo(file)
             if info.is_dir():
-                state.append({'path': file[0:-1], 'is_directory': True})
+                state.append({'path': file[:-1], 'is_directory': True})
             else:
                 state.append(
                     {'path': file, 'is_directory': False,
                      'last_modified': self.get_last_modified_time(info)})
-        # add top level folder
-        if len(name_list) > 0 and name_list[0].split('/')[0] + '/' not in name_list:
-            state.insert(0, {'path':   name_list[0].split(
-                '/')[0][0:-1], 'is_directory': True})
+        # # add top level folder
+        # if len(name_list) > 0 and name_list[0].split('/')[0] + '/' not in name_list:
+        #     state.insert(0, {'path':   name_list[0].split(
+        #         '/')[0], 'is_directory': True})
         self.state_manager.set_state(state)
         return self.state_manager.get_current_state(
         ), self.state_manager.get_previous_state()
-
+    
+    @handle_failure(log)
     def get_last_modified_time(self, zip_info):
         # (year, month, day, hour, minute, second) = zip_info.date_time
         date_string = ''
         for item in zip_info.date_time:
             date_string = date_string + str(item)
         date_time_obj = datetime.datetime.strptime(date_string, '%Y%m%d%H%M%S')
-        return math.floor(date_time_obj.timestamp())
-
+        return math.floor(date_time_obj.timestamp() / 10)
+    
+    @handle_failure(log)
     def read(self, filename):
         for item in self.zip_r.infolist():
             if item.filename == filename:
                 return self.zip_r.read(item.filename).decode('utf-8')
-
+    
+    @handle_failure(log)
     def is_directory(self, filename):
         current_state = self.state_manager.get_current_state()
+        print(current_state)
         for item in current_state:
             if item['path'] == filename:
                 return item['is_directory']
-
+    
+    @handle_failure(log)
     def create_directory(self, filename):
         zfi = ZipInfo(filename + '/')
         self.zip_w.writestr(zfi, '')
         log('Creating directory', filename)
         self.open_read()
         self.open_write()
-
+    
+    @handle_failure(log)
     def delete(self, filename):
-        log("Delete ", filename)
+        is_directory = self.is_directory(filename)
+        log("Delete ", is_directory, filename)
         zout = ZipFile('./temp.zip', 'w')
         for item in self.zip_r.infolist():
             buff = self.zip_r.read(item.filename)
-            if item.filename != filename and item.filename != filename + '/':
+            print(item.filename, filename)
+            if item.filename != filename and (is_directory and not item.filename.startswith(filename)):
                 zout.writestr(item, buff)
         zout.close()
         rename("./temp.zip",
                self.path)
         self.open_read()
-
+    
+    @handle_failure(log)
     def write(self, filename, content):
         zout = ZipFile('./temp.zip', 'w')
         did_write = False
@@ -117,7 +135,8 @@ class Zip:
         rename("./temp.zip",
                self.path)
         self.open_read()
-
+    
+    @handle_failure(log)
     def copy_from(self, class_b, filename):
         target_path = filename
         from_path = filename
